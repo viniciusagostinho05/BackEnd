0a75af58ab9f688562676e0c3780c8adf490ce36
FraccaGerry <4mt2fc9pgv@privaterelay.appleid.com>
2026-09-16 12:32:35 +0200

    Add files via upload

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,1 +1,7 @@
-export const errorHandlers = [];
+import { validationHandler } from './validation-error';
+import { genericErrorHandler } from "./generic";
+import { notFoundHandler } from "./not-found.error";
+import { userExistsHandler } from "./user-exists.error";
+import { alreadyCompletedHandler } from "./already-completed.error";
+
+export const errorHandlers = [validationHandler, notFoundHandler, userExistsHandler, alreadyCompletedHandler, genericErrorHandler];
