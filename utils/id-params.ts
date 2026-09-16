f1f28c94a67acb771363938881e93cf06dd91965
FraccaGerry <4mt2fc9pgv@privaterelay.appleid.com>
2026-09-16 14:33:45 +0200

    Delete utils directory

--- utils/id-params.ts
+++
@@ -1,6 +0,0 @@
-import { IsMongoId } from "class-validator";
-
-export class IdParams {
-  @IsMongoId()
-  id: string;
-}
--- utils/typed-request.ts
+++
@@ -1,7 +0,0 @@
-import { Request } from "express";
-import * as core from "express-serve-static-core";
-
-export interface TypedRequest<
-  B = any,
-  Q = core.Query,
-  P = core.ParamsDictionary> extends Request<P, any, B, Q> { }
